2a222b398aa4fcac8cbcef7bc11c4ec42118bf7c
Batta <Gianlucabb1@gmail.com>
2025-12-15 17:26:05 -0300

    Cosas 1
    No termine

--- AlgoYModelos/Assets/Nivel 1/Scripts/Player/BulletBehaivour.cs
+++ AlgoYModelos/Assets/Nivel 1/Scripts/Player/BulletBehaivour.cs
@@ -7,40 +7,20 @@ public class BulletBehaivour : PlayerWeapon
     [SerializeField] float _speed;
     [SerializeField] float _selfDestroyTime;
     [SerializeField] Vector3 _dir;
-    float _waitDestroy;
-
-    public ObjectPool<BulletBehaivour> _pool;
 
     private void Start()
     { 
-        //Destroy(gameObject, _selfDestroyTime);
-        //Recicle(_pool);
+        Destroy(gameObject, _selfDestroyTime);
     }
 
     void Update()
     {
-        _waitDestroy += Time.deltaTime;
-
-        if(_waitDestroy >= _selfDestroyTime)
-        {
-            _pool.GetBack(this);
-            _waitDestroy = 0;
-        }
-
         Avance();
     }
 
     void Avance()
     {
         transform.position += (transform.forward * _speed * Time.deltaTime);
-    }
-
-    public void Recicle(ObjectPool<BulletBehaivour> pool)
-    {
-        _pool = pool;
-        transform.position = EntitiesManager.Instance.Player.GetComponent<ShootingManager>()._bulletSpawnPoint.position;
-        //transform.localRotation = EntitiesManager.Instance.Player.GetComponent<ShootingManager>()._bulletSpawnPoint.rotation;
-        transform.LookAt(Camera.main.ViewportToWorldPoint(new Vector3(_dir.x, _dir.y, _dir.z)));
     }
 
     private void OnTriggerEnter(Collider other)
@@ -52,27 +32,9 @@ public class BulletBehaivour : PlayerWeapon
                 other.GetComponent<IDamageable>().TakeDamage(_damage);
             }
 
-            //print("choque con " + other.name);
-            _pool.GetBack(this);
-            _waitDestroy = 0;
-            //Destroy(gameObject);
+            Destroy(gameObject);
 
         }
     }
 
-
-    #region Dano
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject.GetComponent<IDamageable>() != null)
-    //    {
-    //        var col = other.gameObject.GetComponent<IDamageable>();
-
-    //        col.TakeDamage(_damage);
-    //    }
-    //    Destroy(gameObject);
-    //    //print("Me choque con " + other.gameObject.name);
-
-    //} 
-    #endregion
 }
--- AlgoYModelos/Assets/Nivel 1/Scripts/Player/ShootingManager.cs
+++ AlgoYModelos/Assets/Nivel 1/Scripts/Player/ShootingManager.cs
@@ -10,42 +10,16 @@ public class ShootingManager : MonoBehaviour
     [SerializeField] Vector3 test;
 
     [SerializeField] BulletBehaivour _bullet;
-    ObjectPool<BulletBehaivour> _poolBullets;
 
-    private void Awake()
-    {
-        _poolBullets = new ObjectPool<BulletBehaivour>(Shoot, BulletOn, BulletOff, 0);
-    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             //Shoot();
-            var bullet = _poolBullets.Get();
-            bullet.transform.position = _bulletSpawnPoint.position;
+
+            Instantiate(_bullet, _bulletSpawnPoint.position, _bulletSpawnPoint.rotation);
         }
     }
 
-    BulletBehaivour Shoot()
-    {
-        return Instantiate(_bullet, _bulletSpawnPoint.position, Quaternion.identity);
-
-        //var bullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity);
-        //bullet.transform.LookAt(Camera.main.ViewportToWorldPoint(new Vector3(test.x, test.y, test.z)));
-
-    }
-
-    void BulletOn(BulletBehaivour bullet)
-    {
-        bullet.gameObject.SetActive(true);
-        bullet.Recicle(_poolBullets);
-
-    }
-
-    void BulletOff(BulletBehaivour bullet)
-    {
-        bullet.Recicle(_poolBullets);
-        bullet.gameObject.SetActive(false);
-    }
 }
